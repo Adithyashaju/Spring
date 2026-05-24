416d7cc7b0ad331862e38682ad9a7cbb23c0c0ff
Adithyashaju <120009793+Adithyashaju@users.noreply.github.com>
2026-05-24 10:38:02 +0530

    Delete booking-service directory

--- booking-service/src/main/java/com/bookashow/bookingservice/repository/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bookashow.bookingservice.repository;
-
-import com.bookashow.bookingservice.entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
